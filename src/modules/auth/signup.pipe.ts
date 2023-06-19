59ef40a7c5bd5ff413826b726480044484267f4b
bmspd <my_spid@mail.ru>
2023-06-19 12:28:30 +0200

    fix(sign-up): new random number generation for username

--- src/modules/auth/signup.pipe.ts
+++ src/modules/auth/signup.pipe.ts
@@ -1,13 +1,23 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { webcrypto } from 'crypto';
 
 @Injectable()
 export class SignupPipe implements PipeTransform {
   constructor(private readonly userService: UsersService) {}
   async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
     if (value.username) return value;
-    // возможно, лучше опереться на рандомную генерацию числа
     const count = (await this.userService.countAll()) + 1;
-    return { ...value, username: `user-${count}` };
+    const username = await this.createUniqueUsername(`user-${count}`);
+    return { ...value, username };
+  }
+  async createUniqueUsername(username: string) {
+    const user = await this.userService.findOneByUsername(username);
+    if (!user) return username;
+
+    const array = new Uint32Array(1);
+    webcrypto.getRandomValues(array);
+    const [number] = array;
+    return this.createUniqueUsername(`user-${number}`);
   }
 }
